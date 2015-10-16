bd8b3bc584e7083e1e240f3f2a9b8176cbc64b0a
Kabir Goel <kabirgoel.kg@gmail.com>
2015-10-16 21:48:25 +0530

    Complete visual overhaul

--- assets/googlefonts.js
+++ assets/googlefonts.js
@@ -1,10 +1,9 @@
 WebFontConfig = {
-  google: { families: [ 'Roboto:300,300italic,400:latin', 'Roboto+Condensed:300italic,400,300:latin' ] }
+  google: { families: [ 'Merriweather:400,300,700:latin', 'Ubuntu+Condensed::latin', 'Lato:300,700:latin' ] }
 };
 (function() {
   var wf = document.createElement('script');
-  wf.src = ('https:' == document.location.protocol ? 'https' : 'http') +
-    '://ajax.googleapis.com/ajax/libs/webfont/1/webfont.js';
+  wf.src = ('https:' == document.location.protocol ? 'https' : 'http') + '://ajax.googleapis.com/ajax/libs/webfont/1/webfont.js';
   wf.type = 'text/javascript';
   wf.async = 'true';
   var s = document.getElementsByTagName('script')[0];
