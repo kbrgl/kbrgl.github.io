0dd03e16aad91b1da31ee03dde4ef6aac81b9873
Kabir Goel <kabirgoel.kg@gmail.com>
2016-01-06 22:06:21 +0530

    Overhaul and add xkcd comics

--- scripts/main.js
+++ scripts/main.js
@@ -1,7 +1,22 @@
 $(
   function(){
+    myBirthday = new Date("March 24, 2002");
+    bdayYear = myBirthday.getFullYear();
+    bday = myBirthday.getDate();
+    bdayMonth = myBirthday.getMonth() + 1;
+    dateToday = new Date();
+    year = dateToday.getFullYear();
+    day = dateToday.getDate();
+    month = dateToday.getMonth() + 1; // +1 because months are 0-indexed
+    myAge = year - bdayYear;
+    if (month < 4)  {
+        if (month === 3 && day < 24 || month < 3) {
+            myAge -= 1;
+        }
+    }
+    ageStr = myAge + " years old";
     $("#occupation").typed({
-            strings: ["web designer", "13 year old", "programmer", "Pythonista", "Rubyist", "Clojurist", "Lisper", "Linux geek", "Ubuntu fan", "pirate"],
+            strings: ["web designer", ageStr, "programmer", "Pythonista", "Rubyist", "Clojurist", "Lisper", "Linux geek", "Ubuntu fan", "pirate"],
             // typing speed
             typeSpeed: 60,
             // time before typing starts
