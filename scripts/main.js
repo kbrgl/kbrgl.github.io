cc6f952b0b91654d9a1f46802268c074c06b32cc
Kabir Goel <kabirgoel.kg@gmail.com>
2016-01-25 19:15:10 +0530

    cleaned up js, randomized comic and cleaned up content

--- scripts/main.js
+++ scripts/main.js
@@ -1,23 +1,21 @@
 $(
   function(){
-    myBirthday = new Date("March 24, 2002");
-    bdayYear = myBirthday.getFullYear();
-    bday = myBirthday.getDate();
-    bdayMonth = myBirthday.getMonth() + 1;
-    dateToday = new Date();
-    year = dateToday.getFullYear();
-    day = dateToday.getDate();
-    month = dateToday.getMonth() + 1; // +1 because months are 0-indexed
-    myAge = year - bdayYear;
-    if (month < 4)  {
-        if ((month === 3 && day < 24) || month < 3) {
-            myAge -= 1;
-        }
+    now = new Date();
+    month = now.getMonth() + 1; // +1 because months are 0-indexed
+    myAge = now.getFullYear() - 2002;
+    if ((month < 4) && ((month === 3 && now.getDate() < 24) || (month < 3)))  {
+        myAge -= 1;
     }
     ageStr = myAge + " year old";
-    $(".js-current-year").html(year);
+    $("#comics").html([
+        '<a href="//xkcd.com/366/"><img class="potd" src="http://imgs.xkcd.com/comics/your_mom.png"/></a>',
+        '<a href="//xkcd.com/676/"><img class="potd" src="http://imgs.xkcd.com/comics/abstraction.png"/></a>',
+        '<a href="//xkcd.com/329/"><img class="potd" src="http://imgs.xkcd.com/comics/turing_test.png"/></a>',
+        '<a href="//xkcd.com/149/"><img class="potd" src="http://imgs.xkcd.com/comics/sandwich.png"/></a>',
+    ][Math.floor(Math.random() * 4)]) // 4 is length of array. if another comic is added, 4 must be changed accordingly.
+    $(".js-current-year").html(now.getFullYear());
     $("#typed").typed({
-            strings: [ageStr, "web designer", "programmer", "Pythonista", "Rubyist", "Clojurist", "Lisper", "Linux geek", "Ubuntu fan", "pirate"],
+            strings: [ageStr, "web designer", "programmer", "Pythonista", "Rubyist", "Clojurist", "Linux geek", "Ubuntu fan", "pirate"],
             // typing speed
             typeSpeed: 50,
             // backspacing speed
