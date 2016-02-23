60f0a36ad29ab6eff2b88570c77ba8769f240239
Kabir Goel <kabirgoel.kg@gmail.com>
2016-02-23 22:10:06 +0530

    Added navbar

--- js/index.js
+++ js/index.js
@@ -1,38 +1,53 @@
-var comics = [
-        '<a href="//xkcd.com/366/"><img class="potd" src="http://imgs.xkcd.com/comics/your_mom.png" alt="xkcd comic strip"/></a>',
-        '<a href="//xkcd.com/676/"><img class="potd" src="http://imgs.xkcd.com/comics/abstraction.png" alt="xkcd comic strip"/></a>',
-        '<a href="//xkcd.com/1597/"><img class="potd" src="https://imgs.xkcd.com/comics/git.png" alt="xkcd comic strip"/></a>',
-        '<a href="//xkcd.com/149/"><img class="potd" src="http://imgs.xkcd.com/comics/sandwich.png" alt="xkcd comic strip"/></a>',
-];
-$(
-    function() {
-        // calculate my current age
-        now = new Date();
-        month = now.getMonth() + 1; // +1 because months are 0-indexed
-        age = now.getFullYear() - 2002;
-        if ((month === 2 && now.getDate() < 24) || (month < 2))  {
-            age -= 1;
-        }
-        ageStr = age + " year old";
+(function ($) {
+	'use strict';
+	var comics = [
+		{
+			href: '//xkcd.com/366/',
+			src: 'http://imgs.xkcd.com/comics/your_mom.png'
+		},
+		{
+			href: '//xkcd.com/676/',
+			src: 'http://imgs.xkcd.com/comics/abstraction.png'
+		},
+		{
+			href: '//xkcd.com/1597/',
+			src: 'https://imgs.xkcd.com/comics/git.png'
+		},
+		{
+			href: '//xkcd.com/149/',
+			src: 'http://imgs.xkcd.com/comics/sandwich.png'
+		}
+	];
 
-        // the typing magic happens here
-        $("#typed").typed({
-            strings: [ageStr, "web designer", "programmer", "Pythonista", "Rubyist", "Clojurist", "Linux geek", "Ubuntu fan", "pirate"],
-            // typing speed
-            typeSpeed: 50,
-            // backspacing speed
-            backSpeed: 20,
-            // time before backspacing
-            backDelay: 500,
-            // loop
-            loop: true,
-            // show cursor
-            showCursor: true,
-            // character for cursor
-            cursorChar: "_",
-        });
+	$(function() {
+		// calculate my current age
+		var now = new Date();
+		var month = now.getMonth() + 1; // +1 because months are 0-indexed
+		var age = now.getFullYear() - 2002;
+		if ((month === 2 && now.getDate() < 24) || (month < 2))  {
+			age -= 1;
+		}
 
-        // randomly choose the xkcd comic to display
-        $("#comics").html(comics[Math.floor(Math.random() * 4)])
-    }
-);
+		// the typing magic happens here
+		$('.typed').typed({
+			strings: [age + ' year old', 'web designer', 'programmer', 'Pythonista', 'Rubyist', 'Clojurist', 'Linux geek', 'Ubuntu fan', 'pirate'],
+			// typing speed
+			typeSpeed: 50,
+			// backspacing speed
+			backSpeed: 20,
+			// time before backspacing
+			backDelay: 500,
+			// loop
+			loop: true,
+			// show cursor
+			showCursor: true,
+			// character for cursor
+			cursorChar: '_',
+		});
+
+		// randomly choose the xkcd comic to display
+		var comic = comics[Math.floor(Math.random() * 4)];
+		var comicDomRepr = $('<a/>', {'href': comic.href, 'class': 'nostyling'}).html($('<img/>', {'src': comic.src, 'class': 'comic'}));
+		$(".xkcd").html(comicDomRepr);
+	});
+})(jQuery);
