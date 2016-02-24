47effe1589d9945d1ab73c651afc565edcb871ae
Kabir Goel <kabirgoel.kg@gmail.com>
2016-02-24 14:06:45 +0530

    fix birthday code

--- js/index.js
+++ js/index.js
@@ -22,7 +22,7 @@
 	$(function() {
 		// calculate my current age
 		var now = new Date();
-		var month = now.getMonth() + 1; // +1 because months are 0-indexed
+		var month = now.getMonth();
 		var age = now.getFullYear() - 2002;
 		if ((month === 2 && now.getDate() < 24) || (month < 2))  {
 			age -= 1;
