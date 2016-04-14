80748b26cc85af1ce41fb880afb2cb6400af02bd
Kabir Goel <kabirgoel.kg@gmail.com>
2016-04-14 12:12:38 +0530

    beta

--- js/index.js
+++ js/index.js
@@ -1,24 +1,5 @@
 (function ($) {
 	'use strict';
-	var comics = [
-		{
-			href: '//xkcd.com/366/',
-			src: 'http://imgs.xkcd.com/comics/your_mom.png'
-		},
-		{
-			href: '//xkcd.com/676/',
-			src: 'http://imgs.xkcd.com/comics/abstraction.png'
-		},
-		{
-			href: '//xkcd.com/1597/',
-			src: 'https://imgs.xkcd.com/comics/git.png'
-		},
-		{
-			href: '//xkcd.com/149/',
-			src: 'http://imgs.xkcd.com/comics/sandwich.png'
-		}
-	];
-
 	$(function() {
 		// calculate my current age
 		var now = new Date();
@@ -28,26 +9,8 @@
 			age -= 1;
 		}
 
-		// the typing magic happens here
-		$('.typed').typed({
-			strings: [age + ' year old', 'web developer', 'programmer', 'Pythonista', 'Rubyist', 'lisper', 'Linux geek', 'Ubuntu fan', 'pirate'],
-			// typing speed
-			typeSpeed: 50,
-			// backspacing speed
-			backSpeed: 20,
-			// time before backspacing
-			backDelay: 500,
-			// loop
-			loop: true,
-			// show cursor
-			showCursor: true,
-			// character for cursor
-			cursorChar: '_',
-		});
-
-		// randomly choose the xkcd comic to display
-		var comic = comics[Math.floor(Math.random() * 4)];
-		var comicDomRepr = $('<a/>', {'href': comic.href, 'class': 'nostyling'}).html($('<img/>', {'src': comic.src, 'class': 'comic'}));
-		$(".xkcd").html(comicDomRepr);
+		$('.scroll-section').scrollIndicatorBullets({
+      titleSelector: 'h1,h2'
+    });
 	});
 })(jQuery);
