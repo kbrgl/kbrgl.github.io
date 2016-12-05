e165b809c03bc99f70a1a42329993da9a5b7d68e
Kabir Goel <kabirgoel.kg@gmail.com>
2016-12-05 16:15:57 +0530

    Added toggleable minimalist design and reduce font sizes

--- js/index.js
+++ js/index.js
@@ -1,1 +1,11 @@
-
+$(function() {
+    $('h1').click(function() {
+        if ($('html').hasClass('colorful')) {
+            $('html').removeClass('colorful');
+            $('html').addClass('minimal');
+        } else if ($('html').hasClass('minimal')) {
+            $('html').removeClass('minimal');
+            $('html').addClass('colorful');
+        }
+    });
+});
