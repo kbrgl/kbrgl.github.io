0f4b57e8445aabeabad12a9d680a3f337caf359b
Kabir Goel <kabirgoel.kg@gmail.com>
2018-11-26 11:32:34 +0530

    Change card style

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,13 +1,18 @@
 import styled from 'styled-components'
 
 const Card = styled.div`
-  margin-left: -30px;
-  margin-right: -30px;
-  padding: 30px;
-  border: 2px solid #3d29f511;
+  margin-left: -10px;
+  margin-right: -10px;
+  margin-bottom: 1.8rem;
+  padding: 30px 20px;
+  border: 1px solid #eaeaef;
   border-radius: 10px;
-  box-shadow: 0 1px 12px 0 #3d29f511;
-  background-image: radial-gradient(circle at top left, #fff, #fafaff);
+  box-shadow: 0 1px 12px 0 #eaeaef11;
+  background: #fff;
+
+  &:last-of-type {
+    margin-bottom: 0;
+  }
 `
 
 export default Card
