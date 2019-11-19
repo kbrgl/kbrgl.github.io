ac15c3ba604b1b94aeef2f2fa831e2545bbb27cb
Kabir Goel <kabirgoel.kg@gmail.com>
2019-11-20 04:18:42 +0530

    Rename layout folder

---
+++ src/components/layout/index.js
@@ -0,0 +1,43 @@
+import React from 'react'
+import Helmet from 'react-helmet'
+import PropTypes from 'prop-types'
+import 'typeface-ibm-plex-sans'
+import 'typeface-ibm-plex-serif'
+
+import Navbar from '../navbar'
+import { Container } from '../grid'
+import Footer from '../footer'
+
+import './normalize.css'
+import './skeleton.css'
+import styles from './styles.module.css'
+
+const Layout = ({ children }) => (
+  <div className={styles.wrapper}>
+    <Helmet
+      title="Kabir Goel"
+      meta={[
+        {
+          name: 'description',
+          content: '17 year old maker from New Delhi, India.',
+        },
+        {
+          name: 'google-site-verification',
+          content: 'mYCbMoGP2dt071GwtAtMtstdSx4tkrIBffu2E2fGbOA',
+        },
+      ]}
+      htmlAttributes={{ lang: 'en' }}
+    />
+    <Container>
+      <Navbar />
+      <main>{children}</main>
+      <hr style={{ marginTop: '8rem' }} />
+      <Footer />
+    </Container>
+  </div>
+)
+Layout.propTypes = {
+  children: PropTypes.node.isRequired,
+}
+
+export default Layout
