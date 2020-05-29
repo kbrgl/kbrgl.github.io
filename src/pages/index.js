1e5b3cd4ee2dfabcc13cc375f87b22d3e5b806b4
Kabir Goel <kabirgoel.kg@gmail.com>
2020-05-29 18:39:07 +0530

    Rewrite homepage copy

--- src/pages/index.js
+++ src/pages/index.js
@@ -20,43 +20,39 @@ const IndexPage = ({ data }) => (
     <Row>
       <Column size="six" offset="three">
         <h1 style={{ marginTop: '8rem' }}>
-          Hey there! I’m a 17 year old maker from New Delhi, India.
+          Hey there! I’m Kabir, a maker from New Delhi, India.
         </h1>
         <p>
-          I enjoy identifying problems around myself and solving them with code.
-          I’m currently working on <a href="https://unslant.org">Unslant</a>, a
-          browser extension aimed at exposing news readers to different
-          perspectives on news stories.
+          I love thinking about computers in terms of the ways we interact with
+          them, from user interfaces to window managers to programming
+          languages. I’m always trying to get better at making minimalist but
+          functional interfaces that make people happy.
         </p>
         <p>
-          Design is a big part of everything I build; lately, I’ve been putting
-          greater emphasis on design-only work. I also love working with text.
-          Typography’s been a big focus of my learning this year&mdash;you can
-          see examples in most of my recent projects. (I’m still a beginner, but
-          I’m trying to get better!)
+          <a href="https://unslant.org">Unslant</a>, a browser extension aimed
+          at exposing news readers to different perspectives on news stories, is
+          my latest project, and stems from my interest in the role of
+          technology in political polarization.
         </p>
         <p>
-          I’m the President of <a href="https://exunclan.com">Exun Clan</a>, the
-          technology club at Delhi Public School R.K. Puram.
+          I’m the outgoing President of{' '}
+          <a href="https://exunclan.com">Exun Clan</a>, the technology club at
+          Delhi Public School R.K. Puram. Many of my fondest memories from high
+          school are of my time in Exun: winning hackathons, putting up our
+          national technology festival, and meeting new people at competitions
+          around New Delhi.
         </p>
         <p>
-          In April, I spent an extremely exciting week at the finals summit of
-          the <a href="https://conradchallenge.org">Conrad Challenge</a>, where
-          my team, <a href="https://katanglabs.com">Katang Labs</a>, won the
-          NASA Goddard award.
+          In fall, I’m going to be headed to the University of California,
+          Berkeley, where I’m planning to major in computer science and (maybe)
+          double major in political science.
         </p>
         <p>
           I haven’t had a whole lot of time to read this year, but I recently
-          finished <em>Zero to One</em> by Peter Thiel and Blake Masters,{' '}
-          <em>The Elements of Typographic Style</em> by Robert Bringhurst, and{' '}
-          <em>Born a Crime</em> by Trevor Noah.
+          finished <em>Super Pumped: The Battle for Uber</em> by Mike Isaac.
         </p>
         <p>
-          Some shows I recently binged include <em>Terrace House Tokyo</em> and{' '}
-          <em>Living with Yourself</em>.
-        </p>
-        <p>
-          Yes, I have a <a href="https://krandiash.github.io">brother</a>.
+          I have a <a href="https://krandiash.github.io">brother</a>.
         </p>
         <p>
           Sometimes <a href="https://twitter.com/KabirGoel">I tweet</a>.
