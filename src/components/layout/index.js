1f136b3d59f83ad29a845df25e49d56a4c24055d
Kabir Goel <kabirgoel.kg@gmail.com>
2020-05-29 19:03:31 +0530

    Update packages

--- src/components/layout/index.js
+++ src/components/layout/index.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import Helmet from 'react-helmet'
+import { Helmet } from 'react-helmet'
 import PropTypes from 'prop-types'
 import 'typeface-ibm-plex-sans'
 import 'typeface-ibm-plex-serif'
